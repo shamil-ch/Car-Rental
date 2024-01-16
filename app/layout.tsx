dd2bf6389993763747b1acf8d72fdb2c73e35eda
shamil-ch <shamilkolekkara@gmail.com>
2024-01-16 14:22:28 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import './globals.css'
+
+
+export const metadata = {
+  title: 'Car Hub',
+  description: 'Discover the best car',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
